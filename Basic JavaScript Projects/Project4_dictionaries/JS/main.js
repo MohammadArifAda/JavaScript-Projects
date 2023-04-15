37c1487bbcc4ffdddffe80d1efad8369c5190583
MohammadArifAda <128553071+MohammadArifAda@users.noreply.github.com>
2023-04-15 14:44:54 -0700

    Change sound to color

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary() {
+  var Animal = {
+    Species:"Dog",
+    Color:"Black",
+    Bread:"Labrador",
+    Age:5,
+    Sound:"Bark!",
+  };
+  document.getElementById("Dictionary").innerHTML = Animal.Color;
+}
